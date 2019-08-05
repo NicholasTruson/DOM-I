c68ef70310f338b21efd2e96e91f0d372134dbc4
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-05 15:54:16 -0700

    final

--- js/index.js
+++ js/index.js
@@ -38,5 +38,106 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Image of a code snippet
+
+let img = document.getElementById("cta-img");
+img.setAttribute('src', siteContent["cta"]["img-src"])
+
+// Navigation
+
+let navA = document.querySelectorAll('a')
+
+navA[0].setAttribute('test', '.nav-item-1')
+navA[0].text = 'Services'
+navA[1].setAttribute('test', '.nav-item-2')
+navA[1].text = 'Product'
+navA[2].setAttribute('test', '.nav-item-3')
+navA[2].text = 'Vision'
+navA[3].setAttribute('test', '.nav-item-4')
+navA[3].text = 'Features'
+navA[4].setAttribute('test', '.nav-item-5')
+navA[4].text = 'About'
+navA[5].setAttribute('test', '.nav-item-6')
+navA[5].text = 'Contact'
+
+// Header
+
+let mainHeader = document.querySelector('h1')
+mainHeader.textContent = siteContent['cta']['h1']
+
+let mainButton = document.querySelector('button')
+mainButton.innerText = "Get Started"
+
+let logo2 = document.querySelector("#cta-img")
+logo2.setAttribute('src', siteContent["cta"]["img-src"])
+
+// Main Content
+
+let heads = document.querySelectorAll('h4')
+
+heads[0].setAttribute ('class', siteContent['main-content']['features-h4'])
+heads[0].textContent = siteContent['main-content']['features-h4']
+heads[1].setAttribute ('class', siteContent['main-content']['about-h4'])
+heads[1].textContent = siteContent['main-content']['about-h4']
+heads[2].setAttribute ('class', siteContent['main-content']['services-h4'])
+heads[2].textContent = siteContent ['main-content']['services-h4']
+heads[3].setAttribute ('class', siteContent['main-content']['vision-h4'])
+heads[3].textContent = siteContent ['main-content']['vision-h4']
+heads[4].setAttribute ('class', siteContent['main-content']['contact-h4'])
+heads[4].textContent = siteContent ['main-content']['contact-h4']
+
+let text = document.querySelectorAll('p');
+
+text[0].setAttribute ('class', siteContent['main-content']['features-content'])
+text[0].textContent = siteContent['main-content']['features-content']
+text[1].setAttribute ('class', siteContent['main-content']['about-content'])
+text[1].textContent = siteContent['main-content']['about-content']
+text[2].setAttribute ('class', siteContent['main-content']['services-content'])
+text[2].textContent = siteContent['main-content']['services-content']
+text[3].setAttribute ('class', siteContent['main-content']['vision-content'])
+text[3].textContent = siteContent['main-content']['vision-content']
+text[4].setAttribute ('class', siteContent['main-content']['contact-content'])
+text[4].textContent = siteContent['main-content']['contact-content']
+
+// Middle Image
+
+let midImg = document.querySelector("#middle-img")
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// Contact
+
+let contactInfo = document.querySelector('.contact')
+
+contactInfo.getElementsByTagName('h4')[0].textContent = siteContent["contact"]["contact-h4"]
+contactInfo.getElementsByTagName('p')[0].textContent = siteContent["contact"]["address"]
+contactInfo.getElementsByTagName('p')[1].textContent = siteContent["contact"]["phone"]
+contactInfo.getElementsByTagName('p')[2].textContent = siteContent["contact"]["email"]
+
+// Footer
+
+let footer = document.querySelector('footer p')
+
+footer.textContent = siteContent ["footer"]["copyright"]
+
+// Navigation Color Change
+
+navA.forEach(link => link.style.color = "green")
+
+// New Navigation Items
+
+const newNav1 = document.createElement('a')
+const mainNav = document.querySelector('nav')
+newNav1.innerText = 'Home'
+newNav1.href = '#'
+mainNav.prepend(newNav1)
+newNav1.style.color = "green"
+
+const newNav2 = document.createElement('a')
+newNav2.innerText = 'Reviews'
+newNav2.href = '#'
+mainNav.append(newNav2)
+newNav2.style.color = "green"
